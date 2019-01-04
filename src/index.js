0f9a629c0b3fd81403d895e518480e688399b0ad
philopian <philopian@users.noreply.github.com>
2019-01-04 13:01:41 -0800

    added a checkbox and updated the select

--- src/index.js
+++ src/index.js
@@ -4,27 +4,40 @@ import { withFormik, Form, Field } from "formik";
 
 import "./styles.css";
 
-const App = ({ values, handleChange }) => (
+const App = ({ values }) => (
   <div className="App">
     <p>Hello Formik</p>
     <Form>
-      <Field component="select" name="color">
-        <option value="red">Red</option>
-        <option value="green">Green</option>
-        <option value="blue">Blue</option>
-      </Field>
+      <label>
+        <span> pick a plan </span>
+        <Field component="select" name="plan">
+          <option value="free">free</option>
+          <option value="basic">basic</option>
+          <option value="premium">premium</option>
+        </Field>
+      </label>
+      <br />
       <Field type="email" name="email" placeholder="Email" />
+      <br />
       <Field type="password" name="password" placeholder="Password" />
+      <br />
+      <label>
+        <span> Join our newsletter </span>
+        <Field type="checkbox" name="newsletter" checked={values.newsletter} />
+      </label>
+      <br />
       <button type="submit">Submit</button>
     </Form>
   </div>
 );
 
 const FormikApp = withFormik({
-  mapPropsToValues({ email, password }) {
+  mapPropsToValues({ email, password, newsletter, plan }) {
     return {
       email: email || "",
-      password: password || ""
+      password: password || "",
+      newsletter: newsletter || true,
+      plan: plan || "basic"
     };
   },
   handleSubmit(values) {
